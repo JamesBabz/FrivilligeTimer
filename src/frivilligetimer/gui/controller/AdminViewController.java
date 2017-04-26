7e513e4095dd4e8fd61593dc485260ab0177c9ad
Jens Nissen <nissenjens@hotmail.com>
2017-04-26 13:02:30 +0200

    i put a logo on AdminView
    i put a logo on AdminView

--- src/frivilligetimer/gui/controller/AdminViewController.java
+++ src/frivilligetimer/gui/controller/AdminViewController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -27,13 +29,22 @@ public class AdminViewController implements Initializable {
     private TableView<?> tbhlaug;
     @FXML
     private MenuBar btnmenu;
+    @FXML
+    private ImageView imageLogo;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       setLogo();
+    } 
     
+    private void setLogo()
+    {
+        Image imageMlogo = new Image("frivilligetimer/gui/image/Mlogo.png");
+        imageLogo.setImage(imageMlogo);
+//        imageLogo.setFitHeight(80);
+//        imageLogo.setFitWidth(150);
+    }
 }
