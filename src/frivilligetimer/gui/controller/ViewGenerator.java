992f88e74be2923e05f8d78cd2560f7cb1d03c01
frodzet <simon.birkedal@gmail.com>
2017-04-27 10:37:06 +0200

    Added Javadoc

--- src/frivilligetimer/gui/controller/ViewGenerator.java
+++ src/frivilligetimer/gui/controller/ViewGenerator.java
@@ -22,11 +22,22 @@ public class ViewGenerator
         this.primaryStage = primaryStage;
     }
 
-    public void generateView(String viewPath, boolean decorated) throws IOException
+    /**
+     * Generates a new view.
+     * @param viewPath The viewpath to the new view schema. e.g. frivilligetimer/gui/view/AddVolunteer.fxml
+     * Note: the .fxml extension must be added to the viewpath string.
+     * @param closeCurrent Whether the primary window this new view is opened upon should be closed.
+     * @param decorated Whether the newly generated view should have a decoration (A Window Border) or not.
+     * @throws IOException 
+     */
+    public void generateView(String viewPath, boolean closeCurrent, boolean decorated) throws IOException
     {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(viewPath));
         Parent root = loader.load();
-        primaryStage.close();
+        if (closeCurrent)
+        {
+            primaryStage.close();
+        }
 
         if (decorated)
         {
