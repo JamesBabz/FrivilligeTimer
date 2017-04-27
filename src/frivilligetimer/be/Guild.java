abb487cd66b062bbd33bf22f5aa8cfeb8e81a0df
thomasmeyerh <thom953b@easv365.dk>
2017-04-27 12:40:50 +0200

    Now with guilds in GUI
    Now with guilds in GUI
    database is now with guild and keys has been given

--- src/frivilligetimer/be/Guild.java
+++ src/frivilligetimer/be/Guild.java
@@ -11,5 +11,34 @@ package frivilligetimer.be;
  */
 public class Guild
 {
-    
+private int id;
+private String name;
+
+    public Guild(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+
 }
