2a5dab2d7c1d3a5d956eaec29c33afd870025dde
frodzet <simon.birkedal@gmail.com>
2017-05-22 10:55:12 +0200

    Refactored and added javadoc

--- src/frivilligetimer/be/Person.java
+++ src/frivilligetimer/be/Person.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package frivilligetimer.be;
 
 import java.awt.image.BufferedImage;
@@ -20,20 +15,18 @@ public abstract class Person
     private final StringProperty firstName = new SimpleStringProperty();
     private final StringProperty lastName = new SimpleStringProperty();
     private final StringProperty fullName = new SimpleStringProperty();
-
     private String phoneNum;
     private String email;
     private BufferedImage image;
 
-
     /**
-     * The contructor
+     * The default contructor
      *
-     * @param id Gets the id of the person
-     * @param firstName Gets the firstname of the person
-     * @param lastName Gets the lastname of the person
-     * @param phoneNum Gets the phonenumber of the person
-     * @param email Gets the emailadress of the person
+     * @param id Sets the id of the person
+     * @param firstName Sets the firstname of the person
+     * @param lastName Sets the lastname of the person
+     * @param phoneNum Sets the phonenumber of the person
+     * @param email Sets the emailadress of the person
      */
     public Person(int id, String firstName, String lastName, String phoneNum, String email, BufferedImage image)
     {
@@ -46,86 +39,163 @@ public abstract class Person
         this.image = image;
     }
 
+    /**
+     * Gets the id of this person.
+     * @return the id.
+     */
     public int getId()
     {
         return id;
     }
 
+    /**
+     * Sets the id of this person.
+     * @param id The id to assign to this person.
+     */
     public void setId(int id)
     {
         this.id = id;
     }
 
+    /**
+     * Gets the first name of this person.
+     * @return the first name.
+     */
     public String getFirstName()
     {
         return firstName.get();
     }
 
+    /**
+     * Sets the first name of this person.
+     * @param value The first name to assign to this person.
+     */
     public void setFirstName(String value)
     {
         firstName.set(value);
     }
 
+    /**
+     * Gets the first name of this person represented by an observable String
+     * Property
+     * @return a string property that represents this person's first name.
+     */
     public StringProperty firstNameProperty()
     {
         return firstName;
     }
 
+    /**
+     * Gets the last name of this person.
+     * @return the last name.
+     */
     public String getLastName()
     {
         return lastName.get();
     }
 
-    public void setLastName(String value)
+    /**
+     * Sets the last name of this person.
+     * @param lastName A string value representing this person's last name.
+     */
+    public void setLastName(String lastName)
     {
-        lastName.set(value);
+        this.lastName.set(lastName);
     }
-
+    
+    /**
+     * Gets the last name of this person represented by an observable String
+     * Property
+     *
+     * @return a string property that represents this person's last name.
+     */
     public StringProperty lastNameProperty()
     {
         return lastName;
     }
 
+    /**
+     * Gets the phone number of this person.
+     * @return the phone number represented by a string.
+     */
     public String getPhoneNum()
     {
         return phoneNum;
     }
 
+    /**
+     * Sets this person's phone number.
+     * @param phoneNum The phone number to be assigned to this person.
+     */
     public void setPhoneNum(String phoneNum)
     {
         this.phoneNum = phoneNum;
     }
-
+    
+    /**
+     * Gets the e-mail of this person.
+     *
+     * @return the e-mail.
+     */
     public String getEmail()
     {
         return email;
     }
 
+    /**
+     * Sets this person's e-mail.
+     *
+     * @param email The e-mail to be assigned to this person.
+     */
     public void setEmail(String email)
     {
         this.email = email;
     }
-
+    
+    /**
+     * Gets the full name of this person.
+     *
+     * @return the full name.
+     */
     public String getFullName()
     {
         return fullName.get();
     }
 
-    public void setFullName(String value)
+    /**
+     * Sets the full name of this person.
+     *
+     * @param fullName A string value representing this person's full name.
+     */
+    public void setFullName(String fullName)
     {
-        fullName.set(value);
+        this.fullName.set(fullName);
     }
 
+    /**
+     * Gets the full name of this person represented by an observable String
+     * Property
+     *
+     * @return a string property that represents this person's full name.
+     */
     public StringProperty fullNameProperty()
     {
         return fullName;
     }
-    
+
+    /**
+     * Gets the profile picture of this person.
+     * @return a buffered image representing this person's profile picture.
+     */
     public BufferedImage getImage()
     {
         return image;
     }
 
+    /**
+     * Sets the profile picture of this person.
+     * @param image A buffered image representing this person's profile picture.
+     */
     public void setImage(BufferedImage image)
     {
         this.image = image;
